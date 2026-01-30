4f78d3d809377d7db2e7e5797f5ac23d173083eb
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2026-01-30 15:42:23 +0200

    fix(utils): use expect for hardcoded principals

--- pipeline/src/utils.rs
+++ pipeline/src/utils.rs
@@ -3,15 +3,15 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use candid::{Nat, Principal};
 
 pub fn max_for_ledger(token: &Principal) -> Nat {
-    if *token == Principal::from_text("ryjl3-tyaaa-aaaaa-aaaba-cai").unwrap() {
+    if *token == Principal::from_text("ryjl3-tyaaa-aaaaa-aaaba-cai").expect("invalid ICP ledger principal") {
         return Nat::from(u64::MAX);
     }
 
-    if *token == Principal::from_text("cngnf-vqaaa-aaaar-qag4q-cai").unwrap() {
+    if *token == Principal::from_text("cngnf-vqaaa-aaaar-qag4q-cai").expect("invalid ckUSDT ledger principal") {
         return Nat::from(340_282_366_920_938_463_463_374_607_431_768_211_455u128);
     }
 
-    if *token == Principal::from_text("mxzaz-hqaaa-aaaar-qaada-cai").unwrap() {
+    if *token == Principal::from_text("mxzaz-hqaaa-aaaar-qaada-cai").expect("invalid ckBTC ledger principal") {
         return Nat::from(u64::MAX);
     }
 
